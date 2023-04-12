748d86a5b7bc929250b2ee7e9c86aef361bcdb7d
Christian Pearce <cotblvd@gmail.com>
2023-04-12 08:08:30 -0600

    magic

---
+++ script.js
@@ -0,0 +1,37 @@
+const answers = [
+  'It is certain',
+  'It is decidedly so',
+  'Without a doubt',
+  'Yes – definitely',
+  'You may rely on it',
+  'As I see it, yes',
+  'Most likely',
+  'Outlook good',
+  'Yes',
+  'Signs point to yes',
+  'Reply hazy, try again',
+  'Better not tell you now',
+  'Ask again later',
+  'Cannot predict now',
+  'Concentrate and ask again',
+  'Don\'t count on it',
+  'Outlook not so good',
+  'My sources say no',
+  'Very doubtful'
+];
+
+const ball = document.querySelector('.ball');
+const answerEl = document.querySelector('#answer');
+
+function shakeBall() {
+  ball.classList.add('shake-animation');
+  setTimeout(() => {
+    const randomIndex = Math.floor(Math.random() * answers.length);
+    answerEl.textContent = answers[randomIndex];
+    ball.classList.remove('shake-animation');
+  }, 2000);
+}
+
+document.querySelector('#shake-btn').addEventListener('click', shakeBall);
+
+  
